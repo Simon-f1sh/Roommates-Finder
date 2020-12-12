d017f06b48125628cc12ac4c76aa4ce6eb968105
Yubo Wang <yuw220@lehigh.edu>
2020-12-12 14:02:11 -0500

    before pull

--- frontend/main.js
+++ frontend/main.js
@@ -37,3 +37,9 @@ function loginError() {
     localStorage.uId = 0;
     localStorage.sessionKey = 0;
 }
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
