e9af993f911455c49e6ac808c31f03a610f834f4
Maizer <qpalzmxdrfyved@gmail.com>
2020-12-12 23:43:37 -0500

    fininshed implementing all frontend functionalities

--- frontend/main.js
+++ frontend/main.js
@@ -19,6 +19,12 @@ function loginResponse(data) {
     if (data.mStatus === 'ok') {
         if (typeof(Storage) !== 'undefined') {
             localStorage.uId = data.mData.uId;
+            localStorage.target_uId = data.mData.uId;
+            if (data.mData.uAdmin === 1) {
+                localStorage.isAdmin = "true";
+            } else {
+                localStorage.isAdmin = "false";
+            }
             localStorage.sessionKey = data.mData.sessionKey
         } else {
             console.log("Your browser doesn't support web storage");
@@ -66,6 +72,8 @@ function search() {
 }
 
 function searchRes() {
+    let searchResultWrapper = document.getElementById("searchResultWrapper");
+
     let gender = document.getElementById("gender");
     let tidiness = document.getElementById("tidiness");
     let noise = document.getElementById("noise");
@@ -74,6 +82,15 @@ function searchRes() {
     let sleep = document.getElementById("sleep");
     let wake = document.getElementById("wake");
 
+    if (gender.value === "" || tidiness.value === "" || noise.value === "" || pet.value === "" || visitor.value === "" || sleep.value === "" || wake.value === "") {
+        document.getElementById("warning").style.display = "block";
+        return;
+    } else {
+        document.getElementById("warning").style.display = "none";
+    }
+
+    searchResultWrapper.style.display = "block";
+
     // make an ajax get
     $.ajax({
         type: "GET",
@@ -81,26 +98,35 @@ function searchRes() {
         url: "/profile?gender=" + gender.value + "&tidiness=" + tidiness.value + "&noise=" + noise.value + "&pet=" + pet.value + "&visitor=" + visitor.value + "&sleep=" + sleep.value + "&wake=" + wake.value,
         dataType: "json",
         success: function(data) {
-            // append info
             let searchResult = document.getElementById("searchResult").getElementsByTagName("tbody")[0];
+
+            // remove all previous info
+            while (searchResult.firstChild) {
+                searchResult.removeChild(searchResult.firstChild);
+            }
+
+            // append info
             for (let i = 0; i < data.mData.length; i++) {
                 let newRow = document.createElement("tr");
                 let newName = document.createElement("td");
                 let newEmail = document.createElement("td");
-                let newBio = document.createElement("td");
+                //let newBio = document.createElement("td");
                 let detail = document.createElement("td");
-                let detal_button = document.createElement("button");
+                let detail_button = document.createElement("button");
                 newName.innerHTML = data.mData[i].uName;
                 newEmail.innerHTML = data.mData[i].uEmail;
-                newBio.innerHTML = "Need to obtain data from backend";
+                //newBio.innerHTML = "Need to obtain data from backend";
                 detail_button.onclick = function() {
                     localStorage.target_uId = data.mData[i].uId;
                     window.location.href = "profile.html";
                 };
                 newRow.appendChild(newName);
                 newRow.appendChild(newEmail);
-                newRow.appendChild(newBio);
-                searchResult.appendChild(newRow)
+                //newRow.appendChild(newBio);
+                detail_button.innerHTML = "Go";
+                detail.appendChild(detail_button);
+                newRow.appendChild(detail);
+                searchResult.appendChild(newRow);
             }
         },
         error: function() {
@@ -129,6 +155,10 @@ function closeForm() {
     let wake = document.getElementById("wake");
     let hobby = document.getElementById("hobby");
 
+    if (name.value === "" || hobby === "" || gender.value === "" || tidiness.value === "" || noise.value === "" || pet.value === "" || visitor.value === "" || sleep.value === "" || wake.value === "") {
+        return;
+    }
+
     // make an ajax put
     $.ajax({
         type: "PUT",
@@ -141,4 +171,20 @@ function closeForm() {
     });
 
     window.location.reload();
+}
+
+function deleteUser() {
+    console.log("deleting user" + localStorage.target_uId);
+    // make an ajax delete
+    $.ajax({
+        type: "DELETE",
+        url: "/profile/" + localStorage.target_uId,
+        dataType: "json",
+        success: function() {
+            window.location.href = "/search.html";
+        },
+        error: function() {
+            console.log("faild to delete this user");
+        }
+    });
 }
